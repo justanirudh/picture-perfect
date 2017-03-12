ebdf22946c4e147154dcaaf7d09572d13210e2b9
JustAnirudh <anirudh.pathak1@gmail.com>
2017-03-12 00:13:10 -0500

    implemented Symbol Table. No tests to test the Symbol table

--- src/cop5556sp17/SymbolTable.java
+++ src/cop5556sp17/SymbolTable.java
@@ -1,43 +1,98 @@
 package cop5556sp17;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
+
 import cop5556sp17.AST.Dec;
 
 public class SymbolTable {
 
-	// TODO add fields
+	private int currentScope, nextScope;
+	private Stack<Integer> scopeStack;
+	private class Attr {
+		int scope;
+		Dec dec;
+		Attr(int scope, Dec dec) {
+			this.scope = scope;
+			this.dec = dec;
+		}
+	}
+	private HashMap<String, ArrayList<Attr>> table;
 
 	/**
 	 * to be called when block entered
 	 */
 	public void enterScope() {
-		// TODO: IMPLEMENT THIS
+		currentScope = nextScope++;
+		scopeStack.push(currentScope);
 	}
 
 	/**
 	 * leaves scope
 	 */
 	public void leaveScope() {
-		// TODO: IMPLEMENT THIS
+		// TODO: Make sure this is correct; diff in slides
+		scopeStack.pop();
+		currentScope = scopeStack.peek();
 	}
 
 	public boolean insert(String ident, Dec dec) {
-		// TODO: IMPLEMENT THIS
-		return true;
+		Attr newAttr = new Attr(currentScope, dec);
+		if (table.containsKey(ident)) {
+			// this ident already declared. First check whether it has not been declared in current scope.
+			for (Attr attr : table.get(ident)) {
+				if (attr.scope == currentScope) // found an ident already in the table with the same scope as current scope
+					return false;
+			}
+			// not declared in current scope, then just add.
+			table.get(ident).add(0, newAttr);// prepending to list of attributes
+			return true;
+		} else {
+			// new ident declared in any scope
+			ArrayList<Attr> list = new ArrayList<>();
+			list.add(newAttr);
+			table.put(ident, list);
+			return true;
+		}
 	}
 
 	public Dec lookup(String ident) {
-		// TODO: IMPLEMENT THIS
-		return null;
+		if (!table.containsKey(ident))
+			return null;
+		ArrayList<Attr> declarations = table.get(ident);
+		Dec dec = null;
+		int minDiff = Integer.MAX_VALUE;
+		for (Attr attr : declarations) {
+			int currDiff = currentScope - attr.scope; // current scope always >= some previously declared scope
+			if (currDiff == 0) // if scope of the declaration of the ident same as the current scope, we got what we wanted
+				return attr.dec;
+			if (currDiff < minDiff) {
+				minDiff = currDiff;
+				dec = attr.dec;
+			}
+		}
+		return dec;
 	}
 
 	public SymbolTable() {
-		// TODO: IMPLEMENT THIS
+		currentScope = 0;
+		nextScope = 0;
+		scopeStack = new Stack<>();
+		table = new HashMap<>();
 	}
 
 	@Override
 	public String toString() {
-		// TODO: IMPLEMENT THIS
-		return "";
+		StringBuilder sb = new StringBuilder();
+		for (String ident : table.keySet()) {
+			sb.append(ident + " -> ");
+			for (Attr attr : table.get(ident)) {
+				sb.append("( scope: " + attr.scope + ", " + "type: " + attr.dec.getType().kind + "), ");
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 }
